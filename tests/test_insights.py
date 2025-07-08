1222a56db372aa50ce1f3cde3fb65ab4a2349003
Marcio <marciocad@gmail.com>
2025-07-07 23:55:28 -0300

    testes do endpoint insights

--- tests/test_insights.py
+++ tests/test_insights.py
@@ -19,3 +19,56 @@ def test_get_books_top_rated(client, fake_books_in_db):
     assert 'rating' in response.json()['books'][0]
     assert 5.0 == response.json()['books'][0]['rating']  # noqa
     assert 2.0 == response.json()['books'][7]['rating']  # noqa
+
+
+def test_get_stats_books_overview(client, fake_books_in_db):
+    # Arrange
+    prices = [10.0, 15.0, 10.0, 15.0]
+    rates = [5.0, 5.0, 5.0, 4.0]
+    fake_books_in_db(4, rating=Iterator(rates), price=Iterator(prices))
+
+    # Act
+    response = client.get('api/v1/stats/overview/')
+    json_expect_response = {
+        'total_books': 4,
+        'average_price': 12.5,
+        'rating_distribuition': {'4.0': 1, '5.0': 3},
+    }
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == json_expect_response
+
+
+def test_get_stats_books_price_range(client, fake_books_in_db):
+    # Arrange
+    prices = [10.0, 10.24, 10.25, 12.0, 18.30, 22.0, 25.0, 29.80]
+    books = fake_books_in_db(8, price=Iterator(prices))
+    books_ranged = books[2:7]  # posicao de 2 até 6
+
+    # Act
+    response = client.get('api/v1/stats/price-range/?min_price=10.25&max_price=25.0')
+    json_expect_response = {'total': 5, 'books': books_ranged}
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == json_expect_response
+
+
+def test_get_stats_categories_books(client, fake_books_in_db):
+    # Arrange
+    categories = ['Fiction', 'Children', 'Fiction', 'Children', 'Fiction']
+    prices = [12.0, 8.0, 18.0, 10.0, 12.0]
+    fake_books_in_db(5, category=Iterator(categories), price=Iterator(prices))
+
+    # Act
+    response = client.get('api/v1/stats/categories')
+    json_expect_response = {
+        'total_categories': 2,
+        'categories_count_distribution': {'Children': 2, 'Fiction': 3},
+        'categories_avg_price_distribution': {'Children': 9.0, 'Fiction': 14.0},
+    }
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == json_expect_response
